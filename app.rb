13f0151251ff04b2d1289f8fa28c152e32e11b71
Simon Green <simon.green@thielsen.co.uk>
2019-01-24 11:53:49 +0000

    refactor

--- app.rb
+++ app.rb
@@ -2,11 +2,11 @@ require 'sinatra/base'
 
 class Birthday < Sinatra::Base
 
-  def days_till_birthday
-    birthday = Date.new(Date.today.year, @month, @day)
-    birthday += 365 if Date.today >= birthday
-    (birthday - Date.today).to_i
-  end
+def days_till_birthday(month, day)
+  birthday = Date.new(Date.today.year, month, day)
+  birthday += 365 if Date.today >= birthday
+  (birthday - Date.today).to_i
+end
 
 get '/' do
   erb(:index)
@@ -14,9 +14,7 @@ end
 
 post '/birthday' do
   @name = params[:name]
-  @day = params[:day].to_i
-  @month = params[:month].to_i
-  @days_till_birthday = days_till_birthday
+  @days_till_birthday = days_till_birthday(params[:month].to_i, params[:day].to_i)
   erb(:birthday)
 end
 
